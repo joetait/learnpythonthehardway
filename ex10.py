26442dfa2fe1e005ed1e72b7d3b15b94f90d3c0d
joetait <joetait@gmx.com>
2014-06-07 11:59:42 +0100

    completed exercsises 10 and 11

--- ex10.py
+++ ex10.py
@@ -2,11 +2,13 @@ tabby_cat = "\tI'm tabbed in."
 persian_cat = "I'm split\non a line."
 backslash_cat = "I'm \\ a \\ cat."
 
+print 010
+
 fat_cat = '''
 I'll do a list:
 \t* Cat food
 \t* Fishies
-\t* Catnip\n\t* Grass
+\t\t* Catnip\n\t* Grass
 '''
 
 print tabby_cat
