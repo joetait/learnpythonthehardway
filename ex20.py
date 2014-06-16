b2fbc4cedbe7c8ba7edccc0f9ed65c3a2f826f73
joetait <joetait@gmx.com>
2014-06-16 22:45:55 +0100

    completed ex 20

--- ex20.py
+++ ex20.py
@@ -25,21 +25,24 @@ current_file = open(input_file)
 
 print "First let's print the whole file:\n"
 
+# print the contents of the input file 
 print_all(current_file)
 
 print "Now let's rewind, kind of like a tape."
 
+# apply rewind to input file, to reset where it is read from
 rewind(current_file)
 
-print(current_file)
 
 print "Let's print three lines:"
 
+# set current line to be 1, since we will be reading from the first line
 current_line = 1
+# print first line, numbered, using print_a_line function
 print_a_line(current_line, current_file)
 
-current_line = current_line + 1
+current_line +=  1
 print_a_line(current_line, current_file)
 
-current_line = current_line + 1
+current_line +=  1
 print_a_line(current_line, current_file)
