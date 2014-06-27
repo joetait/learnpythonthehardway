9addf67017cd08315cc24ff3adc59a05110df779
joetait <joetait@gmx.com>
2014-06-27 22:13:53 +0100

    Completed exercise 35

--- ex35.py
+++ ex35.py
@@ -4,9 +4,9 @@ def gold_room():
     print "This room is full of gold. How much do you take?"
 
     next = raw_input("> ")
-    if "0" in next or "1" in next:
+    try:
         how_much = int(next)
-    else:
+    except ValueError:
         dead("Man, learn to type a number.")
 
     if how_much < 50:
@@ -36,3 +36,39 @@ def bear_room():
             gold_room()
         else:
             print "I got no idea what the means."
+
+
+def cthulhu_room():
+    print "Here you see the great evil Cthulhu."
+    print"He, it, whatever stares at you and you go insane."
+    print "Do you flee for your life or eat your head?"
+
+    next = raw_input("> ")
+
+    if "flee" in next:
+        start()
+    elif "head" in next:
+        dead("Well that was tasty!")
+    else:
+        cthulhu_room()
+
+
+def dead(why):
+    print why, "Good job!"
+    exit(0)
+
+def start():
+    print "You are in a dark room."
+    print "There is a door to your right and left."
+    print "Which one do you take?"
+
+    next = raw_input("> ")
+
+    if next == "left":
+        bear_room()
+    elif next == "right":
+        cthulhu_room()
+    else:
+        dead("You stumble around the room until you starve.")
+
+start()
